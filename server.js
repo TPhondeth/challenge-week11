047db581a129ab5d2dbd228cb83f7d45672b0f0b
Tony Phondeth <phondeth@gmail.com>
2021-08-31 11:56:47 -0400

    added dependencies

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Sets up the Express app
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
+
+
+
+// Listener
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
