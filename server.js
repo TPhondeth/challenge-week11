5474e2a3ad31d171ca0d8d7e12418dad184b3126
Tony Phondeth <phondeth@gmail.com>
2021-09-02 17:43:14 -0400

    updated HTML routes

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 // API Routes
 app.get('/api/notes', (req, res) => {
     let savedNote = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
